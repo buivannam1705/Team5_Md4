94a87e4ef0c3505c174ef28d67d5de38d2f463e4
Your Name <you@example.com>
2023-02-13 22:32:28 +0700

    ppp

--- CaseModule4/src/main/java/com/casemodule4/service/Imp/AccountService.java
+++ CaseModule4/src/main/java/com/casemodule4/service/Imp/AccountService.java
@@ -4,11 +4,15 @@ package com.casemodule4.service.Imp;
 import com.casemodule4.model.Account;
 import com.casemodule4.repository.IAccountRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService implements UserDetailsService {
@@ -24,5 +28,27 @@ public class AccountService implements UserDetailsService {
     public Account findAccountByUsername(String username){
         return iAccountRepo.findAccountByUsername(username);
     }
+
+    public Page<Account> getPage(Pageable pageable) {
+        return iAccountRepo.findAll(pageable);
+    }
+    public List<Account> getAll() {
+        return (List<Account>) iAccountRepo.findAll();
+    }
+
+    public Account save(Account account) {
+        return iAccountRepo.save(account);
+    }
+
+    public Account delete(int id) {
+        Account account = findById(id);
+        iAccountRepo.deleteById(id);
+        return account;
+    }
+
+    public Account findById(int id) {
+        return iAccountRepo.findById(id).get();
+    }
+
 
 }
